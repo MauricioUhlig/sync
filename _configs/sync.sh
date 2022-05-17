c2b9f60577afdf57913627a061b83354988d2001
Mauricio Uhlig <mauriciojastrowuhlig@gmail.com>
2022-05-17 00:54:01 -0300

    sync date: 2022-05-17 00:54:01

--- _configs/sync.sh
+++ _configs/sync.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#Borrowed from anacron
+SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+#End borrowed from anacron
 #echo "Sync path using Git with GitHub"
 
 TODAY=$(date +"%F %H:%M:%S")
 COMPUTER_NAME=$(uname -n)
 COMMIT="sync date: $TODAY - $COMPUTER_NAME"
 
-cd ..
+cd /home/Mauricio/Documents/sync
 git pull 
 
 git add . 
